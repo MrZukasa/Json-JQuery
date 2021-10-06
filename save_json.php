3d51e675c4bbfe56a38692e7d396d41a866af360
Francesco <76690900+MrZukasa@users.noreply.github.com>
2021-10-06 10:11:55 +0200

    Finish

--- save_json.php
+++ save_json.php
@@ -1,6 +1,6 @@
 <?php
       $myFile = "test.json";
-      $fh = fopen($myFile, 'w') or die("can't open file");
+      $fh = fopen($myFile, 'a') or die("can't open file");
       $stringData = $_GET["data"];
       fwrite($fh, $stringData);
       fclose($fh)
